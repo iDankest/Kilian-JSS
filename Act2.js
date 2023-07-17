b91047a3ebce25477aa512aa5b4f782f4c961cc3
iDankest <DankestMirror@hotmail.es>
2023-07-17 17:33:25 +0100

    Actividad2Final

---
+++ Act2.js
@@ -0,0 +1,14 @@
+
+var res = 0;
+var num = parseInt(prompt("Introduce el primer numero"));
+console.log(num);
+var num2 = parseInt(prompt("Introduce el segundo numero"));
+console.log(num2);
+res = num + num2;
+console.log(res);
+res = num - num2;
+console.log(res)
+res = num * num2;
+console.log(res)
+res = num / num2;
+console.log(res);
