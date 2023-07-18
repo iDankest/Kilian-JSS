1ef3fff7864ec27d80d2f0d2d92221e7cd8d826b
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-07-18 10:52:15 +0100

    Update actividad4.js
    MM

--- Actividad 4/actividad4.js
+++ Actividad 4/actividad4.js
@@ -1,10 +1,11 @@
-var niños = parseInt(prompt("Dame numero"));
+var niños = parseInt(prompt("Dame numero"));//cambia Texto por Cantidad de niños
 console.log(niños);
-var niñas = parseInt(prompt("Dame niñas"));
+
+var niñas = parseInt(prompt("Dame niñas"));//cambia Texto por Cantidad de niñas
 console.log(niñas)
 
 proniño = niños*100/(niñas+niños);
-proniña = 100-proniño;
+proniña = 100-proniño;// es una forma de hacerlo lo recomendable realizar la operación
 
 console.log("hay un "+ proniño + " % de niños");
 console.log("hay un "+ proniña + " % de niñas");
