6b2df814e1e092bd3e29b5d73e29e49394d32b11
iDankest <DankestMirror@hotmail.es>
2023-07-20 17:08:35 +0100

    Actividad 12

---
+++ actividad12.js
@@ -0,0 +1,16 @@
+var x = 10;
+var y = 5;
+//Comprobacion de operados
+console.log(x == y); // false
+console.log(x != y); // true
+console.log(x > y); // true
+console.log(x < y); // false
+console.log(x >= y); // true
+console.log(x <= y); // false
+// Operandos
+console.log(x + y); // 15
+console.log(x - y); // 5
+console.log(x * y); // 50
+console.log(x / y); // 2
+console.log(x % y); // 0
+console.log("5" +x)// 510
