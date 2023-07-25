8aedde15c5555dc3907e477593a0c9f36d6ba945
iDankest <DankestMirror@hotmail.es>
2023-07-25 19:50:25 +0100

    Roto

--- ActividadesUA1-Kilian/Actividad 18/sA18.js
+++ ActividadesUA1-Kilian/Actividad 18/sA18.js
@@ -1,20 +1,28 @@
-// function Aform() {
-//     let arr = [];
-
-//     arr.push(document.form.codigo.value);
-//     arr.push(document.form.dni.value);
-//     arr.push(document.form.none.value);
-//     arr.push(document.form.mail.value);
-//     arr.push(document.form.age.value);
-//     arr.push(document.form.seleccion.value);
-//     console.log(arr.join("<br/>"))
-    
-//     document.getElementById("ress").innerHTML = arr.join("<br/>")
-// }
 function Aform() {
-    const arr = Object.values(document.form).map(input => input.value);
-    const result = arr.join("<br/>");
+    let n = parseInt(document.form.nalum.value);
+    let arrb = [[]];
 
-    console.log(result);
-    document.getElementById("ress").innerHTML = result;
+    for(let i=0; i<n; i++){
+        let arr = [];
+        
+        arr.push(document.form.codigo.value);
+        arr.push(document.form.dni.value);
+        arr.push(document.form.none.value);
+        arr.push(document.form.mail.value);
+        arr.push(document.form.age.value);
+        arr.push(document.form.selecion.value);
+
+        arrb.push(arr);
+    }
+
+    console.log(arrb);
+    document.getElementById("ress").innerHTML = arrb.join("<br/>");
 }
+
+// function Aform() {
+//     const arr = Object.values(document.form).map(input => input.value);
+//     const result = arr.join("<br/>");
+
+//     console.log(result);
+//     document.getElementById("ress").innerHTML = result;
+// }
