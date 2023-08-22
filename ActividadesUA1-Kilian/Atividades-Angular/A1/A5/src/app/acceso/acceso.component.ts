745b0cdb7c736840f60981e778602dfa040b6602
Dankest <DankestMirror@hmail.es>
2023-08-22 15:35:40 +0100

    Hola

---
+++ ActividadesUA1-Kilian/Atividades-Angular/A1/A5/src/app/acceso/acceso.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceso',
+  templateUrl: './acceso.component.html',
+  styleUrls: ['./acceso.component.css']
+})
+export class AccesoComponent {
+public imagen : string
+constructor(){
+  this.imagen=''
+}
+}
