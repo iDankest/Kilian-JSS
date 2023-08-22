b4a4b4393038d077d90be32761dfc47363c77dc1
iDankest <kilijes@gmail.com>
2023-08-22 16:46:26 +0100

    Estructura de nueva A5 Angular

---
+++ Angular/A5/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoComponent } from './curso/curso.component';
+import { AlumnosComponent } from './alumnos/alumnos.component';
+import { AlumnoComponent } from './alumno/alumno.component';
+import { ProfesoresComponent } from './profesores/profesores.component';
+import { ProfesorComponent } from './profesor/profesor.component';
+import { AccesoComponent } from './acceso/acceso.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CursosComponent,
+    CursoComponent,
+    AlumnosComponent,
+    AlumnoComponent,
+    ProfesoresComponent,
+    ProfesorComponent,
+    AccesoComponent,
+    RegistroComponent,
+    ConsultaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
