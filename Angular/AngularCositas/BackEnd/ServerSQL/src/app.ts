62a0431728a6b2ca2377bb0533c5ba23ad323eec
Dankest <DankestMirror@hmail.es>
2023-09-26 19:08:57 +0100

    dffd

--- Angular/AngularCositas/BackEnd/ServerSQL/src/app.ts
+++ Angular/AngularCositas/BackEnd/ServerSQL/src/app.ts
@@ -1,6 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import { router } from './routes';
+// import { router } from './routes';
 
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use('/', router);
+
+app.listen(PORT, () => {
+    console.log(`Esto funciona amigo ${PORT}`);
+});
