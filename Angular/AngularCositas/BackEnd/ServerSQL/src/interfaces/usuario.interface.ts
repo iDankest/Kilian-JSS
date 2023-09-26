40867c07366bcd0dbaed6d37c0ef01b5a3a6003a
Dankest <DankestMirror@hmail.es>
2023-09-26 19:53:58 +0100

    a

---
+++ Angular/AngularCositas/BackEnd/ServerSQL/src/interfaces/usuario.interface.ts
@@ -0,0 +1,10 @@
+export interface User{
+    nombre:string;
+    apellidos:string;
+    Fnacimiento:string;
+    email:string
+    telefono:string;
+    tipo:string;
+    foto:string;
+    anotaciones:string;
+}
