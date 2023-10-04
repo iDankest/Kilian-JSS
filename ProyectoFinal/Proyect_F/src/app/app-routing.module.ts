7331602e2980728bdbb802ccc16d22a4c137a193
Dankest <DankestMirror@hmail.es>
2023-10-04 19:31:36 +0100

    PEP

--- ProyectoFinal/Proyect_F/src/app/app-routing.module.ts
+++ ProyectoFinal/Proyect_F/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//
+import { AccederComponent } from './Component/acceder/acceder.component';
+import { ContactoComponent } from './Component/contacto/contacto.component';
+
+const routes: Routes = [
+  {path:'acceder', component:AccederComponent},
+  {path:'contacto', component:ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
