c038e3ee0ad516aae0f75902ca71c1de8bbdef9e
Dankest <DankestMirror@hotmail.es>
2023-10-16 21:24:56 +0100

    2

--- ProyectoFinal/Proyect_F/src/app/app-routing.module.ts
+++ ProyectoFinal/Proyect_F/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccederComponent } from './Component/acceder/acceder.component';
 import { HomeComponent } from './Component/home/home.component';
 import { AgregarArchivoComponent } from './Component/agregar-archivo/agregar-archivo.component';
+import { RegistroComponent } from './Component/registro/registro.component';
+
 
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
   {path: 'acceder', component:AccederComponent},
-  {path: 'subir', component:AgregarArchivoComponent}
+  {path: 'subir', component:AgregarArchivoComponent},
+  {path: 'registro',component:RegistroComponent}
 
 ];
 
