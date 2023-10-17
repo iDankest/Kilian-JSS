206b1200be66eba5d07b7009b53b14f7c9361fc4
Dankest <DankestMirror@hmail.es>
2023-10-17 19:52:28 +0100

    s

--- ProyectoFinal/Proyect_F/src/app/app-routing.module.ts
+++ ProyectoFinal/Proyect_F/src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { AccederComponent } from './Component/acceder/acceder.component';
 import { HomeComponent } from './Component/home/home.component';
 import { AgregarArchivoComponent } from './Component/agregar-archivo/agregar-archivo.component';
 import { RegistroComponent } from './Component/registro/registro.component';
-
+import { ContactoComponent } from './Component/contacto/contacto.component';
 
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
   {path: 'acceder', component:AccederComponent},
   {path: 'subir', component:AgregarArchivoComponent},
-  {path: 'registro',component:RegistroComponent}
+  {path: 'registro',component:RegistroComponent},
+  {path: 'contacto',component:ContactoComponent}
 
 ];
 
