49c4787483328638c11cd9a4ed25d5b9cd6b45a5
Dankest <DankestMirror@hotmail.es>
2023-10-18 23:27:41 +0100

    s

--- ProyectoFinal/Proyect_F/src/app/app-routing.module.ts
+++ ProyectoFinal/Proyect_F/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './Component/home/home.component';
 import { AgregarArchivoComponent } from './Component/agregar-archivo/agregar-archivo.component';
 import { RegistroComponent } from './Component/registro/registro.component';
 import { ContactoComponent } from './Component/contacto/contacto.component';
+import { ConfiguracionComponent } from './Component/configuracion/configuracion.component';
 
 
 const routes: Routes = [
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path: 'acceder', component:AccederComponent},
   {path: 'subir', component:AgregarArchivoComponent},
   {path: 'registro',component:RegistroComponent},
-  {path: 'contacto',component:ContactoComponent}
+  {path: 'contacto',component:ContactoComponent},
+  {path: 'configuracion',component:ConfiguracionComponent}
 
 ];
 
